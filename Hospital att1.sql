813caba464318c8a2ef2f96221f4f09e9500978e
Glaucia Mattos <135081393+Mattos23@users.noreply.github.com>
2023-12-02 13:09:17 -0300

    Add files via upload

---
+++ Hospital att1.sql
@@ -0,0 +1,5 @@
+Use hospital;
+
+ALTER TABLE `medico`
+ADD COLUMN `em_atividade` BOOLEAN NOT NULL DEFAULT 1;
+
---
+++ Hospital att2.sql
@@ -0,0 +1,3 @@
+Use hospital;
+
+UPDATE `medico` SET `em_atividade` = 0 WHERE `id` IN (1, 3, 5, 7, 9);
